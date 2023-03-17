bb4e1d03584c5359bd1dfb7ebb9a1d1d5e81c455
Brittany Burton <114821667+brittanyb89@users.noreply.github.com>
2023-03-16 19:35:01 -0500

    Update client and server code

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,31 +1,25 @@
-import { createBrowserRouter, Link, RouterProvider } from "react-router-dom";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Layout from "./layout";
 import Profiles from "./routes/profiles";
 import Thoughts from "./routes/thoughts";
 
 const router = createBrowserRouter([
   {
-    path: "/profiles",
-    element: <Profiles />,
-  },
-  {
-    path: "/thoughts",
-    element: <Thoughts />,
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "/profiles",
+        element: <Profiles />,
+      },
+      {
+        path: "/thoughts",
+        element: <Thoughts />,
+      },
+    ],
   },
 ]);
 
 export default function App() {
-  return (
-    <RouterProvider router={router}>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/profiles">Profiles</Link>
-          </li>
-          <li>
-            <Link to="/thoughts">Thoughts</Link>
-          </li>
-        </ul>
-      </nav>
-    </RouterProvider>
-  );
+  return <RouterProvider router={router} />;
 }
